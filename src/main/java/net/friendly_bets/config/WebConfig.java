38e97f2f401ea026c76399615dc18e7aeb0f469f
AlexDiz <alexdiz88@gmail.com>
2023-08-07 14:10:23 +0200

    MongoDB settings changing

---
+++ src/main/java/net/friendly_bets/config/WebConfig.java
@@ -0,0 +1,18 @@
+package net.friendly_bets.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
+    }
+}
