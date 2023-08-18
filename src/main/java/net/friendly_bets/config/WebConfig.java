2d217dc65cb0424d8c27c5a3591cebc62d13915c
AlexDiz <alexdiz88@gmail.com>
2023-08-18 17:39:43 +0200

    avatar upload path fix

--- src/main/java/net/friendly_bets/config/WebConfig.java
+++ src/main/java/net/friendly_bets/config/WebConfig.java
@@ -5,8 +5,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-@EnableWebMvc
+// @Configuration
+// @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
