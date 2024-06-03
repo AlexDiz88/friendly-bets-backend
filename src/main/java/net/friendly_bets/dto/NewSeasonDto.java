e5ccc50ce96c64193d2b8888c2b655f99112a45c
alex lobkov <alexdiz88@gmail.com>
2024-06-03 17:23:11 +0200

    fix issue with deleting empty bets

--- src/main/java/net/friendly_bets/dto/NewSeasonDto.java
+++ src/main/java/net/friendly_bets/dto/NewSeasonDto.java
@@ -4,9 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
@@ -18,6 +18,6 @@ public class NewSeasonDto {
     private String title;
 
     @NotNull(message = "{field.isNull}")
-    @Size(min = 1, message = "{betsInSeason.minCount}")
+    @Min(value = 1, message = "{betsInSeason.minCount}")
     private Integer betCountPerMatchDay;
 }
