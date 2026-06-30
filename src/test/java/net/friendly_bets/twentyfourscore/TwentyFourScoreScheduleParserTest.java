95f73996b8e37c0eeea79bdc46497815c67b522a
alex lobkov <alexdiz88@gmail.com>
2026-06-30 03:45:11 +0200

    fix

--- src/test/java/net/friendly_bets/twentyfourscore/TwentyFourScoreScheduleParserTest.java
+++ src/test/java/net/friendly_bets/twentyfourscore/TwentyFourScoreScheduleParserTest.java
@@ -9,6 +9,7 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TwentyFourScoreScheduleParserTest {
