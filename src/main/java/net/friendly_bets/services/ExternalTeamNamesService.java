0c262902574fc88eb8f7dea53d48f950b01dbfbf
alex lobkov <alexdiz88@gmail.com>
2026-07-27 01:48:52 +0200

    remove aiscore

--- src/main/java/net/friendly_bets/services/ExternalTeamNamesService.java
+++ src/main/java/net/friendly_bets/services/ExternalTeamNamesService.java
@@ -1,7 +1,6 @@
 package net.friendly_bets.services;
 
 import lombok.RequiredArgsConstructor;
-import net.friendly_bets.aiscore.AiscoreTeamNamesService;
 import net.friendly_bets.dto.Soccer365TeamNameChipDto;
 import net.friendly_bets.exceptions.BadRequestException;
 import net.friendly_bets.marathonbet.MarathonbetTeamNamesService;
@@ -19,7 +18,6 @@ public class ExternalTeamNamesService {
     private final Soccer365TeamNamesService soccer365TeamNamesService;
     private final MarathonbetTeamNamesService marathonbetTeamNamesService;
     private final TwentyFourScoreTeamNamesService twentyFourScoreTeamNamesService;
-    private final AiscoreTeamNamesService aiscoreTeamNamesService;
 
     public List<Soccer365TeamNameChipDto> fetchUnmappedTeamNames(String providerRaw, String leagueCode) {
         String provider = normalizeProvider(providerRaw);
@@ -28,7 +26,6 @@ public class ExternalTeamNamesService {
             case TeamTitleUtils.MARATHONBET_PROVIDER -> marathonbetTeamNamesService.fetchUnmappedTeamNames(leagueCode);
             case TeamTitleUtils.TWENTYFOUR_SCORE_PROVIDER ->
                     twentyFourScoreTeamNamesService.fetchUnmappedTeamNames(leagueCode);
-            case TeamTitleUtils.AISCORE_PROVIDER -> aiscoreTeamNamesService.fetchUnmappedTeamNames(leagueCode);
             default -> throw new BadRequestException("externalTeamNamesProviderUnsupported");
         };
     }
